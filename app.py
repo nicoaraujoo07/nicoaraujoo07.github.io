af45b9773e6861504bc3838546849f408d6de67f
nicoaraujoo07 <naraujo@fi.uba.ar>
2025-10-02 00:15:56 -0300

    2

--- app.py
+++ app.py
@@ -37,14 +37,14 @@ lista = ["COMPROMISO SOCIAL: Organizamos actividades comunitarias, campañas sol
 "EQUIPO HUMANO: Nuestro cuerpo técnico, profesores y voluntarios comparten una visión común basada en la empatía, la inclusión y el profesionalismo."]
     
 lista2 = [
-    ["Fútbol", "futbol.jpg"],
-    ["Voley", "voley.jpeg"],
-    ["Natación", "natacion.jpeg"],
-    ["Golf", "golf.jpg"],
-    ["Gimnasia artística", "gimnasia-artistica.jpeg"],
-    ["Atletismo", "atletismo.jpeg"],
-    ["Basquet", "basquet.jpeg"],
-    ["Otros...", "otros.jpg"]
+    ["Fútbol ⚽", "futbol.jpg"],
+    ["Voley 🏐", "voley.jpeg"],
+    ["Natación 🤽🏻‍♂️", "natacion.jpeg"],
+    ["Golf ⛳", "golf.jpg"],
+    ["Gimnasia artística 🤸🏻‍♀️", "gimnasia-artistica.jpeg"],
+    ["Atletismo 🏃🏻‍➡️", "atletismo.jpeg"],
+    ["Basquet 🏀", "basquet.jpeg"],
+    ["Otros... ⭐", "otros.jpg"]
     ]
           
 
